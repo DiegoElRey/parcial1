1bf2062a01eb2ec59390bb4228345def99953ba1
DiegoElRey <dandressantiago@unicesar.edu.co>
2020-10-06 09:15:24 -0500

    commit

--- src/app/registro-persona/registro-persona.component.ts
+++ src/app/registro-persona/registro-persona.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { NgModule } from '@angular/core';
 @Component({
   selector: 'app-registro-persona',
   templateUrl: './registro-persona.component.html',
   styleUrls: ['./registro-persona.component.css']
 })
 export class RegistroPersonaComponent implements OnInit {
-
+  nombre: string;
+  apellido: string;
+  sexo: string;
+  edad: string;
   constructor() { }
 
   ngOnInit(): void {
