d7cb0fc4a3c979789216ea2b527c7d21e7983979
DiegoElRey <dandressantiago@unicesar.edu.co>
2020-10-06 09:31:07 -0500

    new commit

---
+++ src/app/modelos/persona.ts
@@ -0,0 +1,10 @@
+export class Persona {
+    identificacion: string;
+    nombre: string;
+    apellido: string;
+    sexo: string;
+    edad: number;
+    departamento: string;
+    ciudad: string;
+    ValorApoyoRecivido: number;
+}
