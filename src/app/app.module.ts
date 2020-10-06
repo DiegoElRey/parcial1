3fea2384b7f869e17160631fd50ec37d5a61f60c
DiegoElRey <dandressantiago@unicesar.edu.co>
2020-10-06 08:33:06 -0500

    segundo commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { RegistroPersonaComponent } from './registro-persona/registro-persona.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    RegistroPersonaComponent
   ],
   imports: [
     BrowserModule,
